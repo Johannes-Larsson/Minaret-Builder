f7d3f2fdebaef2eed4ac8bbdbd1786454674f370
Johannes Larsson <mrja1lisepic@gmail.com>
2015-05-27 12:33:07 +0200

    added the libdx project

---
+++ android/src/com/larsson/johannes/minaretBuilder/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.larsson.johannes.minaretBuilder.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.larsson.johannes.minaretBuilder.Game;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Game(), config);
+	}
+}
