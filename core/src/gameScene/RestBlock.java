6277b521aef4aa95644a000e6acc459d99a10cc3
Johannes-Larsson <mrja1lisepic@gmail.com>
2015-12-17 19:43:58 +0100

    rest block rotation

--- core/src/gameScene/RestBlock.java
+++ core/src/gameScene/RestBlock.java
@@ -2,9 +2,12 @@ package gameScene;
 
 import com.larsson.johannes.minaretBuilder.framework.Animation;
 import com.larsson.johannes.minaretBuilder.framework.GameObject;
+import com.larsson.johannes.minaretBuilder.game.SceneManager;
 
 public class RestBlock extends GameObject {
 
+	private boolean isRight;
+	
 	public RestBlock(boolean isRight, Animation animation, int textureStart, int textureEnd, float x, float y) {
 		super(animation, x, y);
 		animation.sprite.setRegion(textureStart, 0, textureEnd - textureStart, animation.sprite.getTexture().getHeight());
@@ -12,11 +15,16 @@ public class RestBlock extends GameObject {
 		
 		if (isRight) vx = 3;
 		else vx = -3;
+		
+		this.isRight = isRight;
 	}
 	
 	public void update() {
 		super.vy -= .5f;
 		super.update();
+		animation.sprite.setRotation(animation.sprite.getRotation() + (isRight ? -1f : 1f));
+		
+		if (getY() < 100) SceneManager.getCurrentScene().remove.add(this);
 	}
 
 }
